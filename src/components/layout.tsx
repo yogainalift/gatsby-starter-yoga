c58c7b884a42db50dc1f0686d7da30e21fdfbb31
Christos <ctselepidas@gmail.com>
2020-09-20 20:00:38 -0500

    Fix footer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -35,7 +35,7 @@ const Layout: React.FC = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer>
+        <footer style={{ marginTop: '3rem', padding: '1rem 0' }}>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
