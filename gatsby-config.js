42095b3ec4b99b8e7074908374855def88140267
Christos <ctselepidas@gmail.com>
2020-09-20 19:45:27 -0500

    Small cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `gatsby app starter`,
-    description: `Its a gatsby project starter with all the right things: typescript, routes, tailwind`,
+    description: `Its a gatsby project starter with all the right things like typescript and routes`,
     author: `@yogainalift`,
   },
   plugins: [`gatsby-plugin-typescript`, `gatsby-plugin-react-helmet`],
